748118bc47e0fa9cad92769038828ee08950ceab
JCSnap <e0970167@u.nus.edu>
2025-12-16 22:18:56 +0800

    add aoc 2019 day 1 part 2

--- haskell/aoc2019/1.hs
+++ haskell/aoc2019/1.hs
@@ -2,6 +2,9 @@ main :: IO ()
 main = interact solve
 
 solve :: String -> String
-solve input = show $ foldl (\acc x -> acc + x `div` 3 - 2) 0 $ map (\x -> read x :: Int) $ lines input
+solve input = show $ foldl getFuel 0 $ map (\x -> read x :: Int) $ lines input
+  where getFuel acc x
+          | x == 0 = acc
+          | otherwise = let left = maximum [x `div` 3 - 2, 0] in getFuel (acc + left) left
 
 
