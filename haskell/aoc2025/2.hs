8077df69e07a864409c1a4809bf95c1efeb5e93e
JCSnap <e0970167@u.nus.edu>
2025-12-16 15:25:53 +0800

    add day 2 part 2 aoc 2025

--- haskell/aoc2025/2.hs
+++ haskell/aoc2025/2.hs
@@ -10,10 +10,19 @@ splitOnChar delim (c:cs)
       in (c:x) : xs
 
 solve :: String -> String
-solve input = show $ foldl (\acc x -> if isInvalid x then acc + x else acc) 0 $ concat $ map generateRange $ map (\x -> splitOnChar '-' x) $ splitOnChar ',' input
-	where
-		generateRange (x:xs) = [(read x :: Int)..(read $ head xs :: Int)]
-		isInvalid num = even len && take (len `div` 2) numStr == drop (len `div` 2) numStr
-			where
-				numStr = show num
-				len = length $ show num
+solve input = show $ foldl (\acc x -> if isInvalid2 x then acc + x else acc) 0 $ concat $ map generateRange $ map (\x -> splitOnChar '-' x) $ splitOnChar ',' input
+  where
+    generateRange (x:xs) = [(read x :: Int)..(read $ head xs :: Int)]
+    isInvalid num = even len && take (len `div` 2) numStr == drop (len `div` 2) numStr
+      where
+        numStr = show num
+        len = length numStr
+    isInvalid2 num =
+      let s = show num
+          l = length s
+      in any (\k ->
+                k < l &&
+                l `mod` k == 0 &&
+                concat (replicate (l `div` k) (take k s)) == s
+             ) [1 .. l `div` 2]
+
