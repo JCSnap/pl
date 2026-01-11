09cec99715c25238e79e26d628aa8b80500138ed
JCSnap <e0970167@u.nus.edu>
2026-01-12 01:56:02 +0800

    add lasttwo for ocaml

--- ocaml/99problems/tailofalist.ml
+++ ocaml/99problems/tailofalist.ml
@@ -1,8 +1,15 @@
 let rec last lst = match lst with [] -> None | x :: [] -> Some x | _ :: xs -> last xs
 
+let rec last_two lst = match lst with
+  | [] | [_] -> None
+  | [x; y] -> Some (x, y)
+  | _ :: xs -> last_two xs
+
+(* Test cases *)
 
 let test1 = []
 let test2 = [1; 2; 3; 4; 5]
+let test3 = [1]
 
 let () =
   (match last test1 with
@@ -12,4 +19,17 @@ let () =
   (match last test2 with
    | None -> print_endline "Test2: None"
    | Some x -> Printf.printf "Test2: Some %d\n" x)
+
+let () =
+    (match last_two test1 with
+     | None -> print_endline "Test1: None"
+     | Some (x, y) -> Printf.printf "Test1: Some (%d, %d)\n" x y);
+    
+    (match last_two test2 with
+     | None -> print_endline "Test2: None"
+     | Some (x, y) -> Printf.printf "Test2: Some (%d, %d)\n" x y);
+    
+    (match last_two test3 with
+     | None -> print_endline "Test3: None"
+     | Some (x, y) -> Printf.printf "Test3: Some (%d, %d)\n" x y)
 
